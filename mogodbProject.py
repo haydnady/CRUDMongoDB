35a5ca4e2ba1fd427a9909e6e1c7231794a9e976
Haydn <haydnady@gmail.com>
2020-08-02 17:58:05 -0400

    Updated code to work with python 3

--- mogodbProject.py
+++ mogodbProject.py
@@ -5,9 +5,11 @@ from pymongo import MongoClient
 # Making a Connection with MongoClient (using default host and port)
 connection = MongoClient()
 
-# Getting a Database
-db = connection['myDb']
-collection = db['collection']
+#//////////////////////////////////////
+# Getting a Database                //
+db = connection["city"]           #//
+collection = db["inspections"]   #//
+#//////////////////////////////////
 
 # ============================================== Create Docs
 def insert_document(document):
@@ -17,7 +19,7 @@ def insert_document(document):
     try:
         result = collection.insert_one(document)
     except Exception as e:
-        print "\nERROR insert_document:", e
+        print("\nERROR insert_document:", e)
         status = False
         
     return result, status
@@ -29,7 +31,7 @@ def read_document(document):
     try:
         result = collection.find_one(document)
     except Exception as e:
-        print "\nERROR read_document:", e
+        print("\nERROR read_document:", e)
         result = "Failed to retrieve data"
     
     return result
@@ -42,7 +44,7 @@ def update_document(document, update):
         collection.update_one(document, {"$set" : update})
         result = collection.find_one(document)
     except Exception as e:
-        print "\nERROR update_document:", e
+        print("\nERROR update_document:", e)
         result = "Failed to update data"
         
     return result
@@ -55,38 +57,57 @@ def delete_document(document):
         collection.delete_many(document)
         result = collection.find_one(document)
     except Exception as e:
-        print "\nERROR delete_document:", e
+        print("\nERROR delete_document:", e)
         result = "Failed to delete data"
         
     return result
   
 # ============================================== Main
 def main():
-    
-    # Create
-    myDocument = {"_id" : 35445565655, "name" : "test value data OLD"}
-    result, status = insert_document(myDocument)
-    print "\n1. Document Created:", status # Prints status of inserted data (True/False)
-    print "--------------------------------------"
-    
-    # Read
-    result = read_document({"_id" : 35445565655})
-    # Prints JSON of read document
-    print "2. Read Document:\n", json.dumps(result, indent = 4) 
-    print "--------------------------------------"
+  
+    while True:
+      print("==========================================================")
+      userInput = input("1 - Create")
+      userInput = input("2 - Read")
+      userInput = input("3 - Update")
+      userInput = input("4 - Delete")
+      userInput = input("5 - Exit")
       
-    # Update
-    result= update_document({"_id" : 35445565655}, {"name" : "Updated Value UPDATED"})
-    # Prints JSON of updated document
-    print "3. Updated Document:\n", json.dumps(result, indent = 4)
-    print "--------------------------------------"
+      # Create
+      if userInput == 1:
+        userInputDoc = input("Enter document to create: \n")
+        result, status = insert_document(myDocument)
+        print("\n1. Document Created:", status) # Prints status of inserted data (True/False)
+        print("--------------------------------------")
+        
+      # Read
+      elif userInput == 2:
+        userInputDoc = input("Enter document to create: \n")
+        # Prints JSON of read document
+        print("2. Read Document:\n", json.dumps(result, indent = 4))
+        print("--------------------------------------")
+        
+      # Update
+      elif userInput == 3:
+        result= update_document({"_id" : 35445565655}, {"name" : "Updated Value UPDATED"})
+        # Prints JSON of updated document
+        print("3. Updated Document:\n", json.dumps(result, indent = 4))
+        print("--------------------------------------")
+        
+      # Delete  
+      elif userInput == 4: 
+        result = delete_document({"_id" : 35445565655})
+        # Prints "JSON" (null, none) of deleted document
+        print("4. Delete Document:\n", json.dumps(result, indent = 4))
+        print("--------------------------------------")
+        
+      # Exit  
+      elif userInput == 5:
+        break;
       
-    # Delete
-    result = delete_document({"_id" : 35445565655})
-    # Prints "JSON" (null, none) of deleted document
-    print "4. Delete Document:\n", json.dumps(result, indent = 4)
-    print "--------------------------------------"
-      
+      # Default
+      else:
+        print("Option not valid, enter a number from 1 to 4: ")      
 
       
 #//////////////
